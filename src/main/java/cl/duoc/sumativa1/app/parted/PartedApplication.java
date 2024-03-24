7e3e8966adf235154e465600a8cd616744957643
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-24 00:31:56 -0300

    se crea estructura básica

---
+++ src/main/java/cl/duoc/sumativa1/app/parted/PartedApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.sumativa1.app.parted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/sumativa1/app/parted/PartedApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.sumativa1.app.parted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
