46f28abd6e2c5d55a892e8bfca200ec79ee2538b
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-25 00:00:16 -0300

    se crea interface con sus metodos

---
+++ src/main/java/cl/duoc/sumativa1/app/parted/service/SaleService.java
@@ -0,0 +1,19 @@
+package cl.duoc.sumativa1.app.parted.service;
+
+import cl.duoc.sumativa1.app.parted.model.Sale;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.List;
+
+public interface SaleService {
+
+    List<Sale> getDailyEarnings(LocalDate date);
+
+    List<Sale> getMonthlyEarnings(YearMonth yearMonth);
+
+    List<Sale> getYearlyEarnings(Year year);
+
+    List<Sale> getAllSales();
+}
