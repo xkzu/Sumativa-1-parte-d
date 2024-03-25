0cdec407610d6e7edeeee45bf1856e10fbe82f43
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-24 23:59:59 -0300

    se crean endpoints para obtener las ventas por día, mes y año

---
+++ src/main/java/cl/duoc/sumativa1/app/parted/controller/SalesController.java
@@ -0,0 +1,77 @@
+package cl.duoc.sumativa1.app.parted.controller;
+
+import cl.duoc.sumativa1.app.parted.model.Sale;
+import cl.duoc.sumativa1.app.parted.model.SaleResponse;
+import cl.duoc.sumativa1.app.parted.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.List;
+
+@RestController
+public class SalesController {
+
+    private final SaleService saleService;
+
+    @Autowired
+    public SalesController(SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    /*
+        Considerar un microservicio que permita manipular las ventas de una tienda deproductos
+        para mascotas. Considerar las respectivas validaciones necesarias para losdatos que ingresaran.
+        El microservicio debe ser capaz de sacar ganancias diarias,mensuales o anuales a petición de lo solicitado
+    */
+
+    @GetMapping("/earnings/daily/{date}")
+    public ResponseEntity<SaleResponse> getDailyEarnings(
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        List<Sale> dailySales = saleService.getDailyEarnings(date);
+
+        double totalEarnings = dailySales.stream()
+                .mapToDouble(Sale::getAmount)
+                .sum();
+
+        return ResponseEntity.ok(new SaleResponse(dailySales, totalEarnings));
+    }
+
+    @GetMapping("/earnings/monthly/{month}")
+    public ResponseEntity<SaleResponse> getMonthlyEarnings(
+            @PathVariable @DateTimeFormat(pattern = "yyyy-MM") YearMonth month) {
+
+        List<Sale> monthlySales = saleService.getMonthlyEarnings(month);
+
+        double totalEarnings = monthlySales.stream()
+                .mapToDouble(Sale::getAmount)
+                .sum();
+
+        return ResponseEntity.ok(new SaleResponse(monthlySales, totalEarnings));
+    }
+
+    @GetMapping("/earnings/yearly/{year}")
+    public ResponseEntity<SaleResponse> getYearlyEarnings(
+            @PathVariable @DateTimeFormat(pattern = "yyyy-MM") Year year) {
+
+        List<Sale> monthlySales = saleService.getYearlyEarnings(year);
+
+        double totalEarnings = monthlySales.stream()
+                .mapToDouble(Sale::getAmount)
+                .sum();
+
+        return ResponseEntity.ok(new SaleResponse(monthlySales, totalEarnings));
+    }
+
+    @GetMapping("/earnings")
+    public ResponseEntity<List<Sale>> getEarnings() {
+        return ResponseEntity.ok(saleService.getAllSales());
+    }
+}
