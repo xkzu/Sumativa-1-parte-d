6ac67d6e967a97be1b4a275a7f274ff72d017a8d
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 19:54:19 -0400

    se crea repositorio

---
+++ src/main/java/cl/duoc/sumativa1/app/parted/repository/SaleRepository.java
@@ -0,0 +1,7 @@
+package cl.duoc.sumativa1.app.parted.repository;
+
+import cl.duoc.sumativa1.app.parted.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
