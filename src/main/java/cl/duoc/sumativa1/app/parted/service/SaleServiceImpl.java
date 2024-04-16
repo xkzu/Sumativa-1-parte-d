f23e09bf71c1a7911abbee1cc22f096a479a1ce9
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 21:23:45 -0400

    se modifica implementacion getDailyEarnings

--- src/main/java/cl/duoc/sumativa1/app/parted/service/SaleServiceImpl.java
+++ src/main/java/cl/duoc/sumativa1/app/parted/service/SaleServiceImpl.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Year;
 import java.time.YearMonth;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -24,14 +23,9 @@ public class SaleServiceImpl implements SaleService {
 
     @Override
     public List<Sale> getDailyEarnings(LocalDate date) {
-        //se crea una lista y se almacenan las ventas desde nuestra bd en memoria
-        List<Sale> allSales = getSales();
-
-        //se convierte la lista en un stream, luego se aplica un filtro en caso de que la fecha sea correcta
-        //y finalmente lo convierte en lista de nuevo
-        return allSales.stream()
-                .filter(sale -> sale.getSaleTime().toLocalDate().isEqual(date))
-                .toList();
+        LocalDateTime startOfDay = date.atStartOfDay(); // 00:00:00
+        LocalDateTime endOfDay = date.atTime(23, 59, 59, 999999999); // 23:59:59.999999999
+        return saleRepository.findSalesByDate(startOfDay, endOfDay);
     }
 
     @Override
@@ -51,20 +45,4 @@ public class SaleServiceImpl implements SaleService {
         return saleRepository.findAll();
     }
 
-    private List<Sale> getSales() {
-        return Arrays.asList(
-                new Sale(1, "Catnip Toy", 9.99, LocalDateTime.of(
-                        2024, 3, 20, 14, 30)),
-                new Sale(2, "Scratching Post", 25.95, LocalDateTime.of(
-                        2024, 3, 21, 10, 0)),
-                new Sale(3, "Automatic Feeder", 59.99, LocalDateTime.of(
-                        2024, 3, 22, 16, 45)),
-                new Sale(4, "Litter Box", 15.50, LocalDateTime.of(
-                        2024, 4, 1, 11, 20)),
-                new Sale(5, "Collar with Bell", 4.99, LocalDateTime.of(
-                        2024, 4, 1, 12, 30)),
-                new Sale(6, "Gourmet Cat Food, Chicken Flavor", 19.99, LocalDateTime.of(
-                        2024, 4, 2, 9, 15))
-        );
-    }
 }
