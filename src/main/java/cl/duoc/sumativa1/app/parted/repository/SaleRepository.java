cefd2aeb930962c7baf0c58988cbebb4afb57cdd
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 20:39:50 -0400

    se crea query

--- src/main/java/cl/duoc/sumativa1/app/parted/repository/SaleRepository.java
+++ src/main/java/cl/duoc/sumativa1/app/parted/repository/SaleRepository.java
@@ -2,6 +2,12 @@ package cl.duoc.sumativa1.app.parted.repository;
 
 import cl.duoc.sumativa1.app.parted.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query(value = "SELECT s FROM Sale s WHERE EXTRACT(YEAR FROM s.saleTime) = :year", nativeQuery = false)
+    List<Sale> findSalesByYear(int year);
 }
