17900b86a9fbd97055784643e96511731f4217c5
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 21:23:09 -0400

    se crea query findSalesByDate

--- src/main/java/cl/duoc/sumativa1/app/parted/repository/SaleRepository.java
+++ src/main/java/cl/duoc/sumativa1/app/parted/repository/SaleRepository.java
@@ -4,6 +4,7 @@ import cl.duoc.sumativa1.app.parted.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
@@ -13,4 +14,7 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query(value = "SELECT s FROM Sale s WHERE EXTRACT(MONTH FROM s.saleTime) = :month")
     List<Sale> findSalesByMonth(int month);
+
+    @Query("SELECT s FROM Sale s WHERE s.saleTime BETWEEN :startOfDay AND :endOfDay")
+    List<Sale> findSalesByDate(LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
